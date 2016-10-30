1a7641c67c17510812e2a2b96bc0562fc592bdd8
Home <Home@Homes-iMac.local>
2016-10-29 21:46:39 -0400

    Added new files to new branch.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './components/my-component/my-component.component';
+import { MyPipePipe } from './pipes/my-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    MyPipePipe
   ],
   imports: [
     BrowserModule,
