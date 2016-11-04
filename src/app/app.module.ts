c53c5e0df5d06f3b60bcc7e280ae8837cdaf0c05
Harry Cui <harry.cui@yahoo.ca>
2016-11-03 20:41:54 -0400

    Updated package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './components/my-component/my-component.component';
 import { MyPipePipe } from './pipes/my-pipe.pipe';
-<<<<<<< HEAD
 import { LoginComponent } from './components/login/login.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-=======
 import { MyDirectiveDirective } from './directives/my-directive.directive';
->>>>>>> ac12f5e48d915cc2c2eca0d63e11f40343c4ca5a
 
 @NgModule({
   declarations: [
     AppComponent,
     MyComponentComponent,
     MyPipePipe,
-<<<<<<< HEAD
     LoginComponent,
-    NavBarComponent
-=======
+    NavBarComponent,
     MyDirectiveDirective
->>>>>>> ac12f5e48d915cc2c2eca0d63e11f40343c4ca5a
   ],
   imports: [
     BrowserModule,
