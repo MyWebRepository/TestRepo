ce2a88c56f3d791ba703267bb9981e1323f90e39
Harry Cui <harry.cui@craserv.ca>
2016-11-25 10:43:34 -0500

    Updated code.

--- src/app/components/rxjs-example/rxjs-example.component.ts
+++ src/app/components/rxjs-example/rxjs-example.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-rxjs-example',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rxjs-example.component.css']
 })
 export class RxjsExampleComponent implements OnInit {
+  private data: Observable<number>;
+  private values: Array<number> = [];
+  private anyErrors: boolean;
+  private finished: boolean;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  init() {
+    this.data = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(42);
+      }, 1000);
+
+      setTimeout(() => {
+        observer.next(43);
+      }, 2000);
+
+      setTimeout(() => {
+        observer.complete();
+      }, 3000);
+    });
+
+    let subscription = this.data.subscribe(
+      value => this.values.push(value),
+      error => this.anyErrors = true,
+      () => this.finished = true
+    );
   }
 
 }
